4ea12c238308f4f52b64aacb6865dc541ab7377d
Buğra Kadirhan <wintermarstice@gmail.com>
2023-06-19 12:58:54 +0300

    Dealing with tensor shapes...

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
+const utils = @import("utils.zig");
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+export fn _x() void {
+    _ = utils;
 }
