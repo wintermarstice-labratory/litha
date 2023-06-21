6bd38ee0b302220889bbe0579bce6e254bde3ccf
Buğra Kadirhan <wintermarstice@gmail.com>
2023-06-21 14:44:29 +0300

    Add type level checks for generating functions

--- src/utils.zig
+++ src/utils.zig
@@ -34,6 +34,14 @@ pub fn RangeIterator(comptime T: type) type {
 }
 
 inline fn GeneratingFunctionShapeType(comptime F: type) type {
+    // A generating function is a function that returns a scalar for
+    // each coordinate of the tensor. For example, `f(x, y) = xy` is
+    // such a function, filling every (x, y) coordinate with the
+    // value `x * y`.
+    //
+    // A generating function must take N `usize` parameters for coordinates
+    // and must return a scalar.
+
     const ArgsType = std.meta.ArgsTuple(F);
     const RetType = @typeInfo(F).Fn.return_type.?;
 
@@ -49,41 +57,57 @@ inline fn GeneratingFunctionShapeType(comptime F: type) type {
         }
     }
 
-    return [@typeInfo(ArgsType).Struct.fields.len]RetType;
+    return [@typeInfo(ArgsType).Struct.fields.len]usize;
 }
 
-inline fn analyzeGeneratingFunction(comptime func: anytype) void {
-    // A generating function is a function that returns a scalar for
-    // each coordinate of the tensor. For example, `f(x, y) = xy` is
-    // such a function, filling every (x, y) coordinate with the
-    // value `x * y`.
+inline fn GeneratingFunctionOutputStorageType(comptime F: type, comptime shape: anytype) type {
+    // A shape is a N-tuple of usize's describing the shape of the resulting n-d array.
+    // For example, the shape .{ 3, 3 } will require a 2D array, because there are
+    // 2 dimensions specified. The generating function will be evaluated for each
+    // cell coordinate of the array.
     //
-    // A generating function must take N `usize` parameters for coordinates
-    // and must return a scalar.
+    // For example; for a generating function that takes 2 usize's, and returns f32's at each
+    // coordinate, for a shape of .{ 3, 3 }, ought to return a [3][3]f32, where each element
+    // is the generating function value evaluated at corresponding coordinate.
 
-    const FunType = @TypeOf(func);
-    const ArgsType = std.meta.ArgsTuple(FunType);
-    const ReturnType = @typeInfo(FunType).Fn.return_type.?;
+    const ArgsType = std.meta.ArgsTuple(F);
+    const RetType = @typeInfo(F).Fn.return_type.?;
 
-    // Check if the return type is a scalar (fXX, iXX, uXX)
-    if (!comptime std.meta.trait.isNumber(ReturnType)) {
-        @compileError("function is not a valid generating function - " ++ @typeName(ReturnType) ++ " is not a number type");
+    // Let's do some error checking - start by the shape.
+    if (@TypeOf(shape) != ArgsType) {
+        @compileError("shape type does not match generating function's argument tuple type - expected " ++ @typeName(ArgsType) ++ ", found " ++ @typeName(@TypeOf(shape)));
     }
 
-    // Check if the all cooordinates are made of `usize`s.
-    inline for (@typeInfo(ArgsType).Struct.fields, 0..) |T, I| {
-        if (T.type != usize) {
-            @compileError(std.fmt.comptimePrint("function is not a valid generating function - argument number {} is not an 'usize', it is '{s}'", .{ I, @typeName(T) }));
-        }
+    return _CreateNdArrayBackingTypeFromShape(RetType, shape);
+}
+
+inline fn generateNdArrayFromFunctionClosure(comptime Closure: type, comptime shape: anytype) GeneratingFunctionOutputStorageType(@TypeOf(Closure.at), shape) {
+    const Backing = GeneratingFunctionOutputStorageType(@TypeOf(Closure.at), shape);
+
+    var backing: Backing = undefined;
+    _ = backing;
+
+    // TODO: Somehow iterate as much as required as per shape. N for loops needed? (or should I flatten and use divmod chain?)
+    @compileError("not implemented");
+}
+
+inline fn _CreateNdArrayBackingTypeFromShapeArrayAndLength(comptime S: type, comptime N: usize, comptime dimens: [N]usize) type {
+    if (N == 0) {
+        return S;
+    } else {
+        return [dimens[0]]_CreateNdArrayBackingTypeFromShapeArrayAndLength(S, N - 1, dimens[1..]);
     }
+}
+
+inline fn _CreateNdArrayBackingTypeFromShape(comptime S: type, comptime array: anytype) type {
+    const N = @typeInfo(@TypeOf(array)).Array.len;
+    const dimens = @as([N]usize, array);
+    return _CreateNdArrayBackingTypeFromShapeArrayAndLength(S, N, dimens);
 }
 
 test "analyzeGeneratingFunction/fn(usize, usize) f32" {
-    const Closure = struct {
-        pub fn at(i: usize, j: usize) f32 {
-            return @as(f32, i) * @as(f32, j);
-        }
-    };
+    const Actual = GeneratingFunctionShapeType(fn (usize, usize) f32);
+    const Expected = [2]usize;
 
-    analyzeGeneratingFunction(Closure.at);
+    try std.testing.expectEqual(Expected, Actual);
 }
